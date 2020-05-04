70220560ec52ef414102cd67a11159cbebf986b9
Prakash Vaka <prakashkvau@gmail.com>
2020-05-04 07:32:29 -0400

    Matrix 01

---
+++ src/leetcode/Matrix01.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Matrix01 {
+    public int[][] updateMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    queue.offer(new int[] {i, j});
+                }
+                else {
+                    matrix[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] d : dirs) {
+                int r = cell[0] + d[0];
+                int c = cell[1] + d[1];
+                if (r < 0 || r >= m || c < 0 || c >= n || 
+                    matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) continue;
+                queue.add(new int[] {r, c});
+                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
+            }
+        }
+        
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        Matrix01 matrix01 = new Matrix01();
+        int[][] input = new int[][]{{0,0,0}, {0,1,0}, {1,2,1}};
+        System.out.println(matrix01.updateMatrix(input));
+    }
+
+}
