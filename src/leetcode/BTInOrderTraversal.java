71d2bab62dc851fc1635f25d405dd1b6393cbe0b
Prakash Vaka <pvaka@wayfair.com>
2020-05-13 20:45:28 -0400

    BT inOrder traversal.

---
+++ src/leetcode/BTInOrderTraversal.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class BTInOrderTraversal {
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> ret = new ArrayList<>();
+    if (root == null) {
+      return ret;
+    }
+    while (root != null) {
+      if (root.left == null) {
+        ret.add(root.val);
+        root = root.right;
+      } else {
+        TreeNode predecessor = root.left;
+        while (predecessor.right != null) {
+          predecessor = predecessor.right;
+        }
+        predecessor.right = root;
+        TreeNode temp = root;
+        root = root.left;
+        temp.left = null;
+      }
+    }
+    return ret;
+  }
+}
