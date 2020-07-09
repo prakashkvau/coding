3b24736f509d30aedc5f3ab41734cc6067dd9c96
Prakash Vaka <pvaka@wayfair.com>
2020-07-09 06:43:27 -0400

    new code

---
+++ src/IQTest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class IQTest {
+  public static void main(String args[]) {
+    Scanner z = new Scanner(System.in);
+    int a = z.nextInt();
+    int[] b = new int[a];
+    for (int i = 0; i < a; i++) {
+      b[i] = z.nextInt();
+    }
+    int e = 0, o = 0;
+    for (int i = 0; i < a; i++) {
+      if (b[i] % 2 == 0)
+        e++;
+      else
+        o++;
+    }
+    if (e > o) {
+      for (int i = 0; i < a; i++) {
+        if (b[i] % 2 != 0)
+          System.out.println(i + 1);
+      }
+    }
+    if (o > e) {
+      for (int i = 0; i < a; i++) {
+        if (b[i] % 2 == 0)
+          System.out.println(i + 1);
+      }
+    }
+  }
+}
